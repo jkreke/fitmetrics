016cb5ca9633bb01420ad4bbe2704a26df411e1a
jkreke <joseph.kreke@gmail.com>
2016-11-07 10:31:29 -0500

    renaming consistency check to fitmetric check

---
+++ R/fitmetric_check.R
@@ -0,0 +1,45 @@
+fitmetric_check <- function(x,y){
+
+#this routine is only useful when the function is used outside of the fitmetrics package.
+#if the function is used within the package, the routine that calls it generates the arguments beforehand so they're already checked for consistency, class, length,....
+
+#however, the user may wish to use the function outside of fitmetics package and so the arguments need some checking.
+# the function should be written so that there are two matricies that are used in calculations.  #For instance, R2 has x and y or dependent and independent variables.  RMSE also has two #arguments (same, independent and dependent variables).  One may consider these variables "model" and "observation" and it really does not matter which is which.
+
+
+#check classes and dimensions.  If they're vectors, make them matricies of 1xdof dimensions.
+	gclasses <- c("numeric", "integer")
+	
+	if(class(x) %in% gclasses & class(y) %in% gclasses & length(x)==length(y)){
+		x <- matrix(x,nrow=1)
+		y <- matrix(y,nrow=1)
+		
+		} else if(class(x)=="matrix" & class(y)=="matrix"){
+		#dont do anything	
+			
+		} else {
+			stop("x & y classes or lengths different")
+			
+		}
+	if(ncol(x)==1 | ncol(y)==1){stop("can not compute fit with only one value")}
+	
+
+
+#If they're matracies, get their dimensions.  Should be N rows by dof columns.  
+#  N is the number of samples.  dof is the number of degrees of freedom.
+	if(class(x)     == "matrix" & 
+	   class(y)     == "matrix"  &
+	   dim(x)[[1]]  == dim(y)[[1]]  & 
+	   dim(x)[[2]]  == dim(y)[[2]]
+	   ){		
+		N=dim(x)[[1]]						#x and y should be the same (checked above)
+		dof=dim(x)[[2]]						#doesnt matter which is chosen
+		}
+		
+		
+# Return a list of the original inputs (x,y) now 
+#  made into matricies if they were not already, along with N and dof.
+	mlist <- list(x,y,N,dof)
+	return(mlist)
+		
+}
